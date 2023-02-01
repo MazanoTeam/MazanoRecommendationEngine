129bb4d4a98fe4facd303e5baa6cf0d43ea81c1b
Axenos-dev <73383848+Axenos-dev@users.noreply.github.com>
2023-02-01 20:44:38 +0200

    main functionality done

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ from pydantic import BaseModel
 from starlette.responses import JSONResponse
 from dotenv import load_dotenv
 from mongo_connect.connect_to_db import LikesDB
-from recomendations.movies import Recommendations
+from recommendations.movies import Recommendations
+import torch
 import pandas as pd
 import os
 
@@ -14,16 +15,27 @@ app = FastAPI()
 
 load_dotenv()
 DB = LikesDB(os.environ["MONGO_URI"], "mazano", "users")
-MOVIE_VECTORS = pd.read_pickle("recomendations\dataset\movie_vectors.pkl")
+MODEL = torch.load("recommendations\model\model.pt")
+MOVIE_VECTORS = pd.read_pickle("recommendations\dataset\movie_vectors.pkl")
 
 @app.get("/movies/get-recommendations")
 async def movie_recommendations(request: Request):
     likes = DB.get_user_likes(request.uuid, "movie_likes")
-    recs = Recommendations()
+    if not likes:
+        return JSONResponse({
+        "code": 402,
+        "description": "Likes of this user is empty",
+        "recommeandations": None
+    })
+
+    recs = Recommendations([movie['original_title'] for movie in likes], MODEL, MOVIE_VECTORS)
     
+    recs.get_recommendations(10)
+
+    recommendations = [movie['original_title'] for movie in recs.recommendations]
     return JSONResponse({
         "code": 200,
         "description": "Recommendations found",
-        "likes": likes
+        "recommeandations": recommendations
     })
     
