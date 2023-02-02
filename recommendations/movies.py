831e42d6a17fa686ee4546c38f506d7c0a9f134a
Axenos-dev <73383848+Axenos-dev@users.noreply.github.com>
2023-02-02 18:26:22 +0200

    made response as movie objects with random movies

--- recommendations/movies.py
+++ recommendations/movies.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
+import random
+
 
 class Recommendations:
-    def __init__(self, movie_titles: list, model, vectors: pd.DataFrame):
+    def __init__(self, movie_titles: list, model, vectors: pd.DataFrame, total_movie_list: list):
         self.model = model
 
         self.vectors = vectors
+        self.total_movie_list = total_movie_list
         self.recommendations = list()
 
-        self.movie_vectors = [
+        self.liked_movie_vectors = [
             self.vectors['overview'].iloc[vectors.index[vectors['original_title'] == movie_title].tolist()[0]]
             for movie_title in movie_titles
         ][:5]
@@ -22,7 +25,7 @@ class Recommendations:
         return (dot_prod / (norm_a * norm_b))[0][0]
 
     def get_similarities(self, row):
-        for movie_vector in self.movie_vectors:
+        for movie_vector in self.liked_movie_vectors:
             self.recommendations.append(
                 {
                     "original_title": row['original_title'],
@@ -30,12 +33,24 @@ class Recommendations:
                 }
             )
 
-    def get_recommendations(self, n_films: int):
+    def get_recommendations(self, n_films: int, k: float):
+        len_recommended_movies = int(n_films*k)
+
         self.vectors.apply(self.get_similarities, axis=1)
 
         self.recommendations = sorted(
             self.recommendations,
             key=lambda x: x['sim_score'],
             reverse=True
-        )[len(self.movie_vectors):n_films+len(self.movie_vectors)]
+        )[len(self.liked_movie_vectors) : len_recommended_movies]
+
+
+        self.recommendations = [movie['original_title'] for movie in self.recommendations]
+
+        self.recommendations = [movie for movie in self.total_movie_list if movie['original_title'] in self.recommendations]
+
+        len_random_movies = n_films - len(self.recommendations)
+        random_movies = random.sample(self.total_movie_list, len_random_movies)
+
+        self.recommendations = [*self.recommendations, *random_movies]
   
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
 from dotenv import load_dotenv
-from mongo_connect.connect_to_db import LikesDB
+from mongo_connect.connect_to_db import LikesDB, ItemsAPI
 from recommendations.movies import Recommendations
 import torch
 import pandas as pd
@@ -10,17 +10,20 @@ import os
 
 class Request(BaseModel):
     uuid: str
+    n_films: int = 20
     
 app = FastAPI()
 
 load_dotenv()
-DB = LikesDB(os.environ["MONGO_URI"], "mazano", "users")
+USERS_DB = LikesDB(os.environ["MONGO_URI_USERS"], "mazano", "users")
 MODEL = torch.load("recommendations\model\model.pt")
+
+MOVIES = ItemsAPI(os.environ['MONGO_URI_API'], "Mazano-API", "Films-API")
 MOVIE_VECTORS = pd.read_pickle("recommendations\dataset\movie_vectors.pkl")
 
 @app.get("/movies/get-recommendations")
 async def movie_recommendations(request: Request):
-    likes = DB.get_user_likes(request.uuid, "movie_likes")
+    likes = USERS_DB.get_user_likes(request.uuid, "movie_likes")
     if not likes:
         return JSONResponse({
         "code": 402,
@@ -28,14 +31,13 @@ async def movie_recommendations(request: Request):
         "recommeandations": None
     })
 
-    recs = Recommendations([movie['original_title'] for movie in likes], MODEL, MOVIE_VECTORS)
+    recs = Recommendations([movie['original_title'] for movie in likes], MODEL, MOVIE_VECTORS, MOVIES.items)
     
-    recs.get_recommendations(10)
+    recs.get_recommendations(request.n_films, k=0.8)
 
-    recommendations = [movie['original_title'] for movie in recs.recommendations]
     return JSONResponse({
         "code": 200,
         "description": "Recommendations found",
-        "recommeandations": recommendations
+        "recommeandations": recs.recommendations
     })
     
